7b77ea1f040cb6044455bd8c14cfdf36b619c70c
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-10-05 14:35:50 +0100

    app setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import './App.css';
+
+function App() {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="ba3c5588-66f6-46f7-a676-8b9086feda82"
+      userName="dorcas_tunmise"
+      userPassword="123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      />
+  );
+};
+
+export default App;
+
+
---
+++ src/components/TheirMessage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function TheirMessage({ lastMessage, message }) {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+      
+  return (
+        <div className='message-row'>
+          {isFirstMessageByUser && (
+            <div className='message-avatar'
+              style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+            />
+          )}
+          {message?.attachments?.length > 0  
+              ?   
+                  (
+                    <img 
+                      src={message.attachments[0].file}
+                      className="message-image"
+                      style={{ marginRight: isFirstMessageByUser ? `4px` :`48px` }}
+                    />
+                  ) : (
+                          <div className='message' 
+                        style={{ float: 'left', 
+                        backgroundColor: '#cabcdc', 
+                        marginLeft: isFirstMessageByUser ? `4px` :`48px`
+                              }}>
+                          {message.text}
+                        </div>
+                )}      
+          </div>
+  );
+};
+
+export default TheirMessage;
